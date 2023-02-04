8676ae09414e45c94b773bd67a22fcf91af1b898
James <IdiosApps@users.noreply.github.com>
2023-02-04 01:08:00 +0000

    test: [red] can generate a tree from top level - bit more setup

--- test_app.py
+++ test_app.py
@@ -62,16 +62,17 @@ def test_can_filter_toplevel_pipelines(tmp_path):
 def test_can_generate_tree(tmp_path):
     setup_test_files(tmp_path)
 
-    paths = app.list_file_paths(tmp_path)
-
     jenkinsfile = Node("Jenkinsfile")
     a = Node("a", parent=jenkinsfile)
     b = Node("b", parent=jenkinsfile)
     c = Node("c", parent=b)
-
-    for pre, fill, node in RenderTree(jenkinsfile):
-        print("%s%s" % (pre, node.name))
     expected_tree = RenderTree(jenkinsfile)
-    tree = app.generate_tree()
-    
-    assert RenderTree(tree) == expected_tree
+
+    paths = app.list_file_paths(tmp_path)
+    toplevel_files = app.filter_toplevel_files(paths)
+    trees = []
+    for toplevel_path in toplevel_files:
+        trees.append(app.generate_tree(toplevel_path))
+
+    assert len(trees) == 1
+    assert RenderTree(trees[1]) == expected_tree
